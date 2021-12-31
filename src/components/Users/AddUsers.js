6258fd894fa73ce3cb9d024d514903ab3e3052ec
jaresj <justice.jares@gmail.com>
2021-12-31 12:39:48 -0800

    Added a new wrapping component

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import Wrapper from "../Helpers/Wrapper";
 import classes from "./AddUser.module.css";
 import ErrorModal from "../UI/ErrorModal";
 
@@ -43,7 +44,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -70,7 +71,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
